57b7686f70c5390058760c40c795dc60a59d689f
zonkiie <rdatz@gmx.de>
2019-01-01 04:31:38 +0100

    added apache stub functions

--- apache_functions.cpp
+++ apache_functions.cpp
@@ -6,7 +6,8 @@ vector<string> get_all_vhosts()
 	vector<string> vhosts;
 	vector<string> args{"-t", "-D", "DUMP_VHOSTS"};
 	string result = pexec_read("/usr/sbin/apache2ctl", args);
-	regex rex("\\s*port \\d+ namevhost ([^\\s]+) .+");
+	regex rex_vhost("\\s*port \\d+ namevhost ([^\\s]+) .+");
+	regex rex_alias("\\s*alias ([^\\s]+).*");
 	smatch sm;
 	stringstream strs(result);
 	string line;
@@ -15,8 +16,38 @@ vector<string> get_all_vhosts()
 		while(getline(strs, line, '\n'))
 		{
 			if("VirtualHost configuration:" == line) continue;
-			if(regex_match(line, sm, rex)) vhosts.push_back(sm[1]);
+			if(regex_match(line, sm, rex_vhost)) vhosts.push_back(sm[1]);
+			if(regex_match(line, sm, rex_alias)) vhosts.push_back(sm[1]);
 		}
 	}
+	sort (vhosts.begin(), vhosts.end());
+	auto last = unique(vhosts.begin(), vhosts.end());
+	vhosts.erase(last, vhosts.end());
 	return vhosts;
+}
+
+vector<string> get_all_sites()
+{
+	vector<string> sites;
+	return sites;
+}
+
+bool add_site(string sitename)
+{
+	return false;
+}
+
+bool set_site_status(string sitename, bool status)
+{
+	return false;
+}
+
+bool remove_site(string sitename)
+{
+	return false;
+}
+
+bool add_vhost(string sitename, string vhostname)
+{
+	return false;
 }
--- apache_functions.h
+++ apache_functions.h
@@ -2,5 +2,10 @@
 #define __apache_functions__
 
 vector<string> get_all_vhosts();
+vector<string> get_all_sites();
+bool add_site(string sitename);
+bool set_site_status(string sitename, bool status);
+bool remove_site(string sitename);
+bool add_vhost(string sitename, string vhostname);
 
 #endif
